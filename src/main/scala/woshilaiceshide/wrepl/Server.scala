644a9a554c9ffdff2bf15f408ab77d6f3b0a6505
woshilaiceshide <woshilaiceshide@qq.com>
2015-09-12 00:27:53 +0800

    expose the basic http_server to repl'sessions.

--- src/main/scala/woshilaiceshide/wrepl/Server.scala
+++ src/main/scala/woshilaiceshide/wrepl/Server.scala
@@ -8,9 +8,13 @@ import woshilaiceshide.wrepl.util.Utility
 class Server(interface: String, port: Int, parameters: Seq[NamedParam], settings: Settings = Utility.defaultSettings, max_lines_kept_in_output_cache: Int = 32, repl_max_idle_time_in_seconds: Int = 60) {
 
   import woshilaiceshide.wrepl.repl._
+  import scala.tools.nsc.interpreter.NamedParamClass
 
-  val httpServer = new HttpServer(interface, port, taskRunner => {
-    new Bridge(taskRunner, parameters, bridge => {
+  val httpServer: HttpServer = new HttpServer(interface, port, taskRunner => {
+
+    val get_http_server: NamedParamClass = NamedParamClass("get_http_server", "() => woshilaiceshide.wrepl.repl.HttpServer", () => httpServer)
+
+    new Bridge(taskRunner, get_http_server +: parameters, bridge => {
       new PipedRepl(settings, bridge.writer)
     }, max_lines_kept_in_output_cache, repl_max_idle_time_in_seconds)
   })
