804b5546e0f9784e4af9a977535b09ba0debd6b0
woshilaiceshide <woshilaiceshide@qq.com>
2016-02-17 15:58:43 +0000

    for "thinking" ...

--- src/main/scala/woshilaiceshide/wrepl/repl/TypeChecker.scala
+++ src/main/scala/woshilaiceshide/wrepl/repl/TypeChecker.scala
@@ -15,8 +15,9 @@ class TypeChecker(val global: Global) extends Plugin {
   private object Component extends PluginComponent {
     val global: TypeChecker.this.global.type = TypeChecker.this.global
     val runsAfter = List("refchecks")
+    override val runsRightAfter: Option[String] = Some("refchecks")
     // Using the Scala Compiler 2.8.x the runsAfter should be written as below
-    // val runsAfter = List[String]("refchecks");
+    //val runsAfter = List[String]("refchecks");
     val phaseName = TypeChecker.this.name
     override def description = TypeChecker.this.description
     def newPhase(_prev: Phase) = new TypeCheckerPhase(_prev)
@@ -25,10 +26,39 @@ class TypeChecker(val global: Global) extends Plugin {
 
     class TypeCheckerPhase(prev: Phase) extends StdPhase(prev) {
 
+      def show(x: Any) {
+        println(x)
+      }
+
       override def name = TypeChecker.this.name
       def apply(unit: CompilationUnit) {
-        println(s"source: ${unit.source}")
-        println(s"body: ${unit.body}")
+        //println(s"source: ${unit.source}")
+        //println(s"body: ${unit.body}")
+        for (
+          tree @ Apply(Select(rcvr, nme.DIV), List(Literal(Constant(0)))) <- unit.body; if rcvr.tpe <:< definitions.IntClass.tpe
+        ) {
+          println(tree)
+          global.reporter.error(tree.pos, "definitely division by zero")
+        }
+        def clzName(x: Any) = {
+          val raw = x.getClass.getName
+          val i = raw.indexOf('$')
+          raw.substring(i + 1)
+        }
+        println(s"----------${unit}")
+        for (tree <- unit.body) {
+          tree match {
+            case Select(x, y) => {
+              if (x.isInstanceOf[global.Ident] && x.toString == "t") {
+                show(x)
+              }
+
+              println(s"a tree(${clzName(tree)}): ${clzName(x)} -> ${clzName(y)}: ${x} -> ${y}")
+            }
+            case x => //println(s"a tree(${tree.getClass}): ${tree}")
+          }
+
+        }
       }
     }
   }
